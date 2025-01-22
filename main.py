c4cab364d44983b6de0f968c53d33e7ff4621062
CasperD598 <casper.20@hotmail.dk>
2025-01-22 12:51:36 +0100

    0.2: Added function for counting words

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     with open("/home/casper/WORKSPACE/github.com/CasperD598/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+    return file_contents
 
-main()
+def count_words(txt):
+    words = txt.split()
+    number = 0
+    for word in words:
+        number += 1
+    return number
+
+content = main()
+word_number = count_words(content)
+print(word_number)
