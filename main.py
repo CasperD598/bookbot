ccf06b716a67a615fb93076a33889b844e450fc1
CasperD598 <casper.20@hotmail.dk>
2025-01-22 14:51:59 +0100

    1.0: Added function to report on number of character apperances

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     dict = get_num_apperance(text)
-    print(f"{num_words} words found in the document")
-    print(dict)
+    char_list = list(dict.items())
+    char_list.sort(reverse=True, key=sort_on)
+    alpha_list = only_alpha(char_list)
+    
+    print_report(alpha_list, book_path, num_words)
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,5 +26,22 @@ def get_num_apperance(text):
         else:
             Apperance_dict[character] = 1
     return Apperance_dict
+
+def sort_on(dict):
+    return dict[1]
+
+def only_alpha(listt):
+    sorted_list = []
+    for item in listt:
+        if item[0].isalpha():
+            sorted_list.append(item)
+    return sorted_list
+
+def print_report(list, path, num_words):
+    print(f"--- Begin report of {path[53:]} ---")
+    print(f"{num_words} words found in the document")
+    for item in list:
+        print(f"The '{item[0]}' character was found {item[1]} times")
+    print("--- End report ---")
 
 main()
