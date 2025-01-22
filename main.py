6e90915a76c223deba239670e52a9d18a40413c1
CasperD598 <casper.20@hotmail.dk>
2025-01-22 11:57:43 +0100

    0.1: Added books folder and read function to main

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    with open("/home/casper/WORKSPACE/github.com/CasperD598/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+main()
